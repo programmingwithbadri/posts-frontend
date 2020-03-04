3c685e7823d40fa8b65f01d2d583fccae47cb46d
Badri <badri.py@outlook.com>
2020-03-04 20:44:21 +0530

    Getting User Input

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html"
 })
-export class PostCreateComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-
+export class PostCreateComponent {
+  postValue = "";
+  newPost = "";
   onAddPost() {
-    alert("Post Added");
+    this.newPost = this.postValue;
   }
 }
