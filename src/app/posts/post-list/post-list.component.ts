ce263d78b1964459da99532f03d2447dc50831a3
Badri <badri.py@outlook.com>
2020-03-18 23:24:26 +0530

    Outputing posts template

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html'
+})
+export class PostListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
