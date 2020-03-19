4a2c3d44fad961582b887837b14378f73104ff83
Badri <badri.py@outlook.com>
2020-03-19 12:02:44 +0530

    Creating a Post Model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Post } from '../post.model';
 
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html"
 })
 export class PostListComponent implements OnInit {
-  @Input() posts = [];
+  @Input() posts:Post[] = [];
   constructor() {}
 
   ngOnInit(): void {}
