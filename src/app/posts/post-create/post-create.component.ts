59c920aecff9388d56209a5ace12b902200b448c
Badri <badri.py@outlook.com>
2020-04-08 23:20:47 +0530

    Edit the posts by reusing the create post component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,20 +1,49 @@
-import { Component } from "@angular/core";
-import { Post } from "../post.model";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { PostsService } from "../post.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Post } from "../post.model";
 
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"]
+  styleUrls: ["./post-create.component.css"],
 })
-export class PostCreateComponent {
-  constructor(public postsService: PostsService) {}
+export class PostCreateComponent implements OnInit {
+  private mode = "create";
+  private postId: string;
+  post: Post;
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-  onAddPost(form: NgForm) {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.valid) {
-      this.postsService.addPosts(form.value.title, form.value.content);
-      form.resetForm();
+      if (this.mode === "create") {
+        this.postsService.addPosts(form.value.title, form.value.content);
+        form.resetForm();
+      } else {
+        this.postsService.updatePost(
+          this.postId,
+          form.value.title,
+          form.value.content
+        );
+        form.resetForm();
+      }
     }
   }
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -36,6 +36,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return { ...this.posts.find((p) => p.id === id) };
+  }
+
   addPosts(title: string, content: string) {
     const post: Post = {
       id: null,
@@ -60,6 +64,17 @@ export class PostsService {
       .delete<{ message: string }>(`http://localhost:3000/api/posts/${postId}`)
       .subscribe(() => {
         const updatedPosts = this.posts.filter((post) => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id, title, content };
+    this.http
+      .put(`http://localhost:3000/api/posts/${id}`, post)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter((post) => post.id !== id);
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
